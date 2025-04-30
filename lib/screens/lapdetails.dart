051cb52de2a4f9660e10b9d19b60d5e4a6c561f2
Janana <140499994+JananaDanithra22@users.noreply.github.com>
2025-04-30 17:15:20 +0530

    add brand name and category too specifications

--- lib/screens/lapdetails.dart
+++ lib/screens/lapdetails.dart
@@ -81,7 +81,8 @@ class _LaptopDetailsPageState extends State<LaptopDetailsPage> {
       'Graphics: ${laptopData!['graphics'] ?? 'N/A'}',
       'Weight: ${laptopData!['weight'] ?? 'N/A'}',
       'Operating System: ${laptopData!['os'] ?? 'N/A'}',
-      'Brand: ${laptopData!['Brand'] ?? 'N/A'}',
+      'Brand: ${laptopData!['brand'] ?? 'N/A'}',
+      'Category: ${laptopData!['category'] ?? 'N/A'}',
     ];
 
     String price = laptopData!['price'].toString();
