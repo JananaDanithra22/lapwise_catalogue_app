b0f3c03f5531142a36ae04dbf17689c3d692809c
Teammate's Name <140804180+SasindiWijekoon@users.noreply.github.com>
2025-05-25 16:52:03 +0530

    Import Firebase dependencies

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,5 +1,8 @@
 // lib/screens/profile.dart
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
